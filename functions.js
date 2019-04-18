34b4b1f4ef704f0129dc673751a0ea5cf4a302da
moniwang <moniwang@umich.edu>
2019-04-17 23:33:01 -0400

    removed js

--- functions.js
+++
@@ -1,9 +0,0 @@
-function clickMe(){
-    var about=document.getElementById("clicky");
-    if (about.innerHTML=="he's still the big gay"){
-        about.innerHTML="hi";
-    }
-    else if (about.innerHTML=="hi"){
-        about.innerHTML="he's still the big gay";
-    }
-}
