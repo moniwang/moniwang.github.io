af57106ce4f3d69e10836af5ddc9b90a891d0ee5
moniwang <moniwang@umich.edu>
2019-04-17 23:20:09 -0400

    lol

---
+++ functions.js
@@ -0,0 +1,9 @@
+function writeAbout(){
+    var about=document.getElementById("clicky");
+    if (about.innerHTML=="he's still the big gay"){
+        about.innerHTML="";
+    }
+    else if (about.innerHTML==""){
+        about.innerHTML="he's still the big gay";
+    }
+}
