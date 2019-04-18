d4cc916b025251145ab4a586020991d40fbd6546
moniwang <moniwang@umich.edu>
2019-04-17 23:23:27 -0400

    hi

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
 function clickMe(){
     var about=document.getElementById("clicky");
     if (about.innerHTML=="he's still the big gay"){
-        about.innerHTML="";
+        about.innerHTML="hi";
     }
-    else if (about.innerHTML==""){
+    else if (about.innerHTML=="hi"){
         about.innerHTML="he's still the big gay";
     }
 }
